1a1a8c9b36d40eb371e09494d8324f33a5f0d448
Brandon <bampereza@miumg.edu.gt>
2022-05-14 11:15:53 -0600

    VectorMostrarBA

---
+++ ProcesosB.java
@@ -0,0 +1,23 @@
+
+package proyectovectoresb;
+
+/**
+ *
+ * @author BRANDON
+ */
+public class ProcesosB {
+    
+    
+    public void vectorMostrar(){
+        
+    int numerosBA []= new int [100];
+    for(int i=0; i<numerosBA.length; i++){
+    
+    numerosBA [i]=i+1;
+    System.out.println(numerosBA[i]);
+}
+}
+}
+
+      
+  
---
+++ ProyectoVectoresB.java
@@ -0,0 +1,25 @@
+
+package proyectovectoresb;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author BRANDON
+ */
+public class ProyectoVectoresB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+   Scanner entradas = new Scanner(System.in);
+   
+   ProcesosB proyectoVectoresB = new ProcesosB();
+   
+  proyectoVectoresB.vectorMostrar();
+}
+}
+    
+
